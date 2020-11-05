7813b39f501b7e021229eef11b85387695125497
pratyushagarwal22 <agarwalpratyush2222@gmail.com>
2020-11-05 23:57:28 +0530

    Started Java Code

---
+++ java/firstprogram.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class firstprogram
+{
+  public void mymethod()
+  {
+    System.out.println("Hello World!");
+    System.out.println("This is my first Java Program");
+  }
+  public static void main(String Args[])
+  {
+    firstprogram obj = new firstprogram();
+    obj.mymethod();
+  }
+}
